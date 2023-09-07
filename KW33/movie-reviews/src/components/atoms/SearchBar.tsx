edd7ba136ebbf8965d4cc58c09ed6dcee43f6e7c
aabishtkhh <aabish.tariqkhan@noseryoung.com>
2023-09-07 13:44:59 +0200

    needs a push

---
+++ KW33/movie-reviews/src/components/atoms/SearchBar.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { IconButton, InputBase, Paper } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+function SearchBar() {
+  const [inputText, setInputText] = useState("");
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  return (
+    <>
+      <div className="search">
+        <Paper variant="outlined"
+          sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250 }}
+        >
+          <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="find Review"
+          inputProps={{ 'aria-label': 'find review' }}
+
+            onChange={inputHandler}
+          />
+          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </div>
+      {/*braucht daten*/}
+    </>
+  );
+}
+
+export default SearchBar;
---
+++ KW33/movie-reviews/src/components/service/Api.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from "axios";
+
+const BASE_URL = `http://localhost:8080/`; //backend zugreifen
+
+/*damit verweigern wir Redundanz*/
+export const defaultAxiosInstance: AxiosInstance = axios.create({
+    baseURL: BASE_URL,
+})
+
+//interceptor: 
+defaultAxiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig<any>) => {
+    let correctPath: boolean = config.url !== "sign-in";
+    if (localStorage.getItem("accessToken") !== "" && correctPath) {
+        config.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+    }
+    return config;
+    },
+
+    (error: AxiosError) => {
+        return Promise.reject(error);
+    }
+)
