11df77f29b615151ab94f0cf64356ee1071d1198
aabishtkhh <aabish.tariqkhan@noseryoung.com>
2023-09-22 09:26:53 +0200

    sonar cloud/ qube update

--- KW33/GhibliMovieReviews/src/main/java/ch/noseryoung/GhibliMovieReviews/domain/authority/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.noseryoung.GhibliMovieReviews.domain.authority;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
-}
-
--- KW33/GhibliMovieReviews/src/main/java/ch/noseryoung/GhibliMovieReviews/domain/role/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ch.noseryoung.GhibliMovieReviews.domain.role;
-
-import ch.noseryoung.GhibliMovieReviews.domain.authority.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Authority, Integer> {
-}
